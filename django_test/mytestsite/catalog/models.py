daf67d532d67f6aa898138fc9e87bcb6e7ce00c3
jared <jaredm.stutzman@gmail.com>
2024-11-20 07:36:57 -0500

    UPDATE: customize admin site

--- django_test/mytestsite/catalog/models.py
+++ django_test/mytestsite/catalog/models.py
@@ -34,37 +34,6 @@ class Genre(models.Model):
             ),
         ]
 
-class Book(models.Model):
-    """Model representing a book (but not a specific copy of a book)."""
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
-    # Foreign Key used because book can only have one author, but authors can have multiple books.
-    # Author as a string rather than object because it hasn't been declared yet in file.
-
-    content = models.TextField(
-        max_length=100000, help_text="Enter the contents of the book")
-
-    summary = models.TextField(
-        max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN', max_length=13,
-                            unique=True,
-                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
-                                      '">ISBN number</a>')
-
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    genre = models.ManyToManyField(
-        Genre, help_text="Select a genre for this book")
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.title
-
-    def get_absolute_url(self):
-        """Returns the URL to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
-
-import uuid # Required for unique book instances
 
 class Languages(models.Model):
     """Model representing a Language (e.g. English, French, Japanese, etc.)"""
@@ -84,6 +53,46 @@ class Languages(models.Model):
         """String for representing the Model object."""
         return self.name
 
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+    # Foreign Key used because book can only have one author, but authors can have multiple books.
+    # Author as a string rather than object because it hasn't been declared yet in file.
+
+    content = models.TextField(
+        max_length=100000, help_text="Enter the contents of the book")
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book",default = "")
+    language = models.ForeignKey('Languages', on_delete=models.RESTRICT, null=True)
+    isbn = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(
+        Genre, help_text="Select a genre for this book")
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+import uuid # Required for unique book instances
+
 class BookInstance(models.Model):
 
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -91,7 +100,6 @@ class BookInstance(models.Model):
                           help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
-    language = models.ForeignKey('Languages', on_delete=models.RESTRICT, null=True)
     due_back = models.DateField(null=True, blank=True)
 
     LOAN_STATUS = (
